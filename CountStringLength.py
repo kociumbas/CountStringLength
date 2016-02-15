714472e29745c416f1cd31422ce375e4b899f9a0
kociumbas <kociumbas@outlook.com>
2016-02-16 00:25:38 +0100

    CountStringLength

---
+++ CountStringLength.py
@@ -0,0 +1,14 @@
+﻿#define a function that computes the length of a given list and string.
+#(it is true that python has the len() function built in,
+#but writing it yourself is nevertheless a good exercise.)
+
+message=()
+sum=int()
+
+
+message=input("Write the string: ")
+i=int(message.count("i"))
+a=int(message.count("a"))       #do it with all the letters
+letters=int(i+a)
+
+print(letters)
